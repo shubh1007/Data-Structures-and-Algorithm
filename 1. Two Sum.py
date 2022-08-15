852f017ee3fe367bc40ed0d4aaf3253f614b5374
Shubham <92504244+shubh1007@users.noreply.github.com>
2022-08-15 21:57:44 +0530

    Leetcode 75 Day 13

---
+++ 1. Two Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for num1 in range(len(nums)):
+            for num2 in range(num1, len(nums)):
+                if num1 != num2 and nums[num2] == (target - nums[num1]):
+                    return [num1, num2]
+        return []
+                    
