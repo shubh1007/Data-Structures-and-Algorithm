9e02d9e89218ed7a3967479006a15c2568fa7e17
Shubham <92504244+shubh1007@users.noreply.github.com>
2022-08-18 11:32:55 +0530

    DSA Daily Challenge

---
+++ 1338. Reduce Array Size to The Half.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minSetSize(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        n = len(arr)
+        count = Counter(arr).most_common()
+        count.sort(key=lambda c: -c[1])
+
+        sum = 0
+        for i, c in enumerate(count):
+          sum += c[1]
+          if sum >= n // 2:
+            return i + 1
