872c3a9b1582395a1bad5b6812fa17e30858aac2
Shubham <92504244+shubh1007@users.noreply.github.com>
2022-09-27 23:28:55 +0530

    DSA Daily Challenge

---
+++ 838. Push Dominoes.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def pushDominoes(self, d):
+        """
+        :type dominoes: str
+        :rtype: str
+        """
+        d = "L" + d + "R"
+        res = []
+        l = 0
+        for r in range(1, len(d)):
+            if d[r] == '.':
+                continue
+            mid = r - l - 1
+            if l:
+                res.append(d[l])
+            if d[l] == d[r]:
+                res.append(d[l] * mid)
+            elif d[l] == 'L' and d[r] == 'R':
+                res.append('.' * mid)
+            else:
+                res.append('R' * (mid // 2) + '.' * (mid % 2) + 'L' * (mid // 2))
+            l = r
+        return "".join(res)
