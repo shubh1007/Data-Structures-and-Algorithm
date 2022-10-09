c5e0be542385bd47d8b00d2c61771e221bd0a24f
Shubham <92504244+shubh1007@users.noreply.github.com>
2022-10-09 07:16:09 +0530

    DSA Daily Challenge

---
+++ 653. Two Sum IV - Input is a BST.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:   
+        
+        values = []
+        self.inorder_traverse(root, values)
+
+        left = 0
+        right = len(values) - 1
+        
+        while left < right:
+            if values[left] + values[right] < k:
+                left += 1
+            elif values[left] + values[right] > k:
+                right -= 1
+            else:
+                return True
+                    
+        return False
+
+        
+    def inorder_traverse(self, root, results):
+        if not root:
+            return
+        
+        self.inorder_traverse(root.left, results)
+        results.append(root.val)
+        self.inorder_traverse(root.right, results)
