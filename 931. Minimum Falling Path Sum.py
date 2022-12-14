bcc726a61c10c93b8c66fdb41950c4fa38d68098
shubh1007 <b200045@nitsikkim.ac.in>
2022-12-14 10:14:24 +0530

    Self Programmed and documented

--- 931. Minimum Falling Path Sum.py
+++ 931. Minimum Falling Path Sum.py
@@ -1,11 +1,21 @@
 class Solution:
-    def minFallingPathSum(self, matrix):
+    def minFallingPathSum(self, matrix)->int:
+        """Calculates and returns the minimum falling path sum 
+        from first row to the last row of the 2D matrix provided.
+
+        Args:
+            matrix (List[List[int]]): 2D matrix of integer type.
+
+        Returns:
+            int: returns the minimum path sum value from first row to 
+            the last row of the given matrix.
+        """
         for i in range(1, len(matrix)):
             for j in range(len(matrix[0])):
-                l = matrix[i][j - 1] if j - 1 >=0 else float("inf")
-                c = matrix[i - 1][j] 
-                r = matrix[i - 1][j + 1] if j + 1 < len(matrix[0]) else float("inf")
-                matrix[i][j] += min(l, c, r)
+                left = matrix[i - 1][j - 1] if j - 1 >= 0 else float("inf")
+                center = matrix[i - 1][j] 
+                right = matrix[i - 1][j + 1] if j + 1 < len(matrix[0]) else float("inf")
+                matrix[i][j] += min(left, center, right)
         return min(matrix[-1])
 
 matrix = [[2,1,3],[6,5,4],[7,8,9]]
