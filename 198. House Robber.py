64501250460bfcce69fa7d341fb8599ae0000c05
shubh1007 <b200045@nitsikkim.ac.in>
2022-12-14 10:43:21 +0530

    Memoization improvement

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -1,13 +1,15 @@
 class Solution:
     def rob(self, nums):
-        self.track = 0
+        memo = [-1 for _ in range(len(nums) + 1)]
         def helper(i):
+            if memo[i] != -1: return memo[i]
             if i < 0: return 0
-            return max(helper(i - 2) + nums[i], helper(i - 1))
+            result = max(helper(i - 2) + nums[i], helper(i - 1))
+            memo[i] = result
+            return result
         return helper(len(nums) - 1)
-        
 
-nums = [1, 2, 3, 1]
+nums = [2, 7, 9, 3, 1]
 sol = Solution()
 res = sol.rob(nums)
 print(res)
