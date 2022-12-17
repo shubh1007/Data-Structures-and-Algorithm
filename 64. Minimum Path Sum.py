d723757bd7e34a62eb32d70d07aabea04382f784
shubh1007 <b200045@nitsikkim.ac.in>
2022-12-17 18:09:04 +0530

    Self Improvization

--- 64. Minimum Path Sum.py
+++ 64. Minimum Path Sum.py
@@ -4,12 +4,14 @@ class Solution:
     def minPathSum(self, grid):
         m = len(grid)
         n = len(grid[0])
-        res = [[float("inf") for _ in range(n + 1)] for _ in range(m + 1)]
-        res[m][n - 1] = 0
+        for i in grid:
+            i.append(float("inf"))
+        grid.append([float("inf") for _ in range(n + 1)])
+        grid[m][n - 1] = 0
         for row in range(m - 1, -1, -1):
             for col in range(n - 1, -1, -1):
-                res[row][col] = grid[row][col] + min(res[row + 1][col], res[row][col + 1])
-        return res[0][0]
+                grid[row][col] = grid[row][col] + min(grid[row + 1][col], grid[row][col + 1])
+        return grid[0][0]
         
 sol = Solution()
 grid = [[1,3,1],[1,5,1],[4,2,1]]
