c6c01f3253ce2bc2d97cf8853a905883aad355ec
shubh1007 <b200045@nitsikkim.ac.in>
2022-12-19 11:45:56 +0530

    COPIED and understood the concept

--- 8. String to Integer (atoi).py
+++ 8. String to Integer (atoi).py
@@ -1,36 +1,45 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        # STEP 1: Ignoring leading whitespaces
-        # STEP 2: Identify the sign
-        # STEP 3: Identify the number
-        # STEP 4: Break if whitespaces found
-        # STEP 5: Identfy the range
-        MIN_INT = -2**31
-        MAX_INT = 2**31 - 1
-        sign = 1
-        num = 0
-        idx = 0
-        n = len(s)
-        while idx < n and s[idx] == " ":
-            idx += 1
-        if idx < n and s[idx] == "-":
-            sign = -1
-            idx += 1
-        elif idx < n and s[idx] == "+":
-            idx += 1
         
-        while idx < n and s[idx].isdigit():
-            num = (num * 10) + int(s[idx])
-            idx += 1
-        if idx == n:
-            if num < MIN_INT: return MIN_INT
-            if num >= MAX_INT: return MAX_INT
-            return num * sign
-        else:
-            return 0
+        def will_exceed(val, sign, next_val):
+            if sign == 1:
+                if INT_LIMIT_BY_10 < val:
+                    return MAX_INT
+                elif INT_LIMIT_BY_10 == val and next_val and next_val > 7:
+                    return MAX_INT
+                return None
+            else:
 
-sol = Solution()
-s = "words and 987"
-result = sol.myAtoi(s)
-print(result)
+                if INT_LIMIT_BY_10 < val:
+                    return MIN_INT
+                elif INT_LIMIT_BY_10 == val and next_val and next_val > 8:
+                    return MIN_INT
+                return None
+
+        s = s.strip(' ')
         
+        if not s:
+            return 0
+        
+        sign = 1
+        if s[0] in {'+', '-'}:
+            sign = -1 if s[0] == '-' else 1
+            s = s[1:]
+
+        MAX_INT = 2**31 -1
+        MIN_INT = - 2**31
+        INT_LIMIT_BY_10 = 214748364
+        
+        
+        val = 0
+        for i, c in enumerate(s):
+            if not c.isdigit():
+                break
+            next_val = ord(c) - ord('0')
+            print(next_val)
+            limit_val = will_exceed(val, sign, next_val)
+            if limit_val:
+                return limit_val
+            
+            val = val * 10 + next_val
+        return sign * val
