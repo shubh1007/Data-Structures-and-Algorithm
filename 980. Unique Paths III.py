2e9920f004c31f512e2fba80eb3500c3d17f4775
shubh1007 <b200045@nitsikkim.ac.in>
2022-12-31 12:33:44 +0530

    DSA Daily Challenge

--- 980. Unique Paths III.py
+++ 980. Unique Paths III.py
@@ -1,35 +1,40 @@
 class Solution:
     def uniquePathsIII(self, grid):
-        hashMap = {}
+        self.res = 0
         m, n = len(grid), len(grid[0])
-        validCount = 0
-        invalidCount = 0
-        start = (0, 0)
+        start, end = 0, 0
+        count = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1: start = (i, j)
-                elif grid[i][j] == 0: validCount += 1
-                elif grid[i][j] == -1: invalidCount += 1
-                
-        def traverse(i, j):
-            if (i, j) in hashMap: return hashMap[(i, j)]
-            if i < 0 or j < 0 or i == m or j == n: return 0
-            if grid[i][j] == 2: 
-                if validCount 
-                return 1
-            if grid[i][j] == -1: return 0
-            if grid[i][j] == 1: return 1
-            left = traverse(i, j - 1)
-            right = traverse(i, j + 1)
-            up = traverse(i - 1, j)
-            down = traverse(i + 1, j)
-            hashMap[(i, j)] = left + right + up + down
-            validCount += 1
-            return hashMap[(i, j)]
-        return traverse(*start)
+                if grid[i][j] == 1:
+                    start = (i, j)
+                if grid[i][j] == 2:
+                    end = (i, j)
+                if grid[i][j] != -1:
+                    count += 1
 
-grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+        def traverse(i, j, COUNT):
+            if (i < 0 or j < 0 or i == m or j == n):
+                return
+            elif (i, j) == end:
+                if COUNT == count:
+                    self.res += 1
+                return
+            elif grid[i][j] == -1:
+                return
+            # print((i, j), COUNT)
+            grid[i][j] = -1
+            traverse(i, j - 1, COUNT + 1)
+            traverse(i, j + 1, COUNT + 1)
+            traverse(i - 1, j, COUNT + 1)
+            traverse(i + 1, j, COUNT + 1)
+            grid[i][j] = 0
+
+        traverse(start[0], start[1], 1)
+        return self.res
+
+
+grid = [[0,1],[2,0]]
 sol = Solution()
 res = sol.uniquePathsIII(grid)
 print(res)
-
