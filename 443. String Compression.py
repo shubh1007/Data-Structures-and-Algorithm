fe2d4a5d35662cdcd4471b689cb92cd14af560b3
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-02 09:54:36 +0530

    DSA Daily Challenge

---
+++ 443. String Compression.py
@@ -0,0 +1,27 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        res = ""
+        count = 1
+        i = 1
+        while i < len(chars):
+            if chars[i] == chars[i - 1]:
+                count += 1
+            else:
+                if count == 1: 
+                    res += chars[i - 1] 
+                else:
+                    res += chars[i - 1] + str(count)
+                count = 1
+            i += 1
+        if count == 1:
+            res += chars[i - 1]
+        else:
+            res += chars[i - 1] + str(count)
+        for i in range(len(res)):
+            chars[i] = res[i]
+        print(chars)
+        return len(res)
+
+
+
+
