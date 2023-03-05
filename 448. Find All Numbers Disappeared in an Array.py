d403b21ed46a273bf5e97490be038c4cb332a171
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-05 23:13:56 +0530

    Practice Questions

---
+++ 448. Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        return [i for i in range(1, len(nums)+1) if i not in s]
