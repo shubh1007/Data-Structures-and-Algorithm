cd6ee24d88fb86905be65286b9e92039498b2b51
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-09 21:21:01 +0530

    Practice Questions

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -1,21 +1,32 @@
 class Solution:
     def letterCombinations(self, digits):
-        n = list(digits)
         numMap = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz", 
+            0 : "",
+            1 : "",
+            2 : "abc",
+            3 : "def",
+            4 : "ghi",
+            5 : "jkl",
+            6 : "mno",
+            7 : "pqrs",
+            8 : "tuv",
+            9 : "wxyz"
         }
         output = []
-        def backtrack():
-            pass
-
-
+        def backtrack(num, string):
+            if num == "":
+                return
+            if num == 0:
+                output.append(string)
+                return
+            num = int(num)
+            last_ligit = num % 10
+            num = num // 10
+            for i in numMap[last_ligit]:
+                backtrack(num, i + string)
+        backtrack(digits, "")
+        return output
+    
 sol = Solution()
 digits = "23"
 res = sol.letterCombinations(digits)
