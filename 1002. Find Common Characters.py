5b2cf02477a848fa2a121b0f6afd4511947d51c6
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-09 17:52:57 +0530

    Practice Questions

---
+++ 1002. Find Common Characters.py
@@ -0,0 +1,11 @@
+import collections
+from typing import List
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        count = collections.Counter(words[0])
+        for string in words:
+            new_count = collections.Counter(string)
+            for k in count:
+                count[k] = min(count[k], new_count[k])
+        return count.elements()
+
