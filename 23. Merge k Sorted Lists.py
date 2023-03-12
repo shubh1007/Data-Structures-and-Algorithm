3a8fff15d50093bf07c4cff01f51a7ffab30b6d7
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-12 09:50:51 +0530

    DSA Daily Challenge

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -1,2 +1,21 @@
+class ListNode:
+    def __init__(self, x = 0):
+        self.val = x
+        self.next = None
 class Solution:
     def mergeKLists(self, lists):
+        nums = []
+        for head in lists:
+            while head:
+                nums.append(head)
+                temp = head
+                head = head.next
+                temp.next = None
+        nums.sort(key = lambda x: x.val)
+        head = ListNode()
+        temp = head
+        for node in nums:
+            temp.next = node
+            temp = temp.next
+        return head.next
+        
