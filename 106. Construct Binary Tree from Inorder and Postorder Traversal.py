9dcf209e6ec4c26fbf51ca12db5ed7aecf649e96
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-16 08:06:58 +0530

    DSA Daily Challenge

--- 106. Construct Binary Tree from Inorder and Postorder Traversal.py
+++ 106. Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -1,21 +1,47 @@
+from typing import List, Optional
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def buildTree(self, inorder, postorder):
-        hashmap = {}
-        for idx, val in enumerate(inorder):hashmap[val] = idx
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        # stores index of inorder elements 
+        # inorder_elements : index
+        hm = {}
+        for idx, val in enumerate(inorder):
+            hm[val] = idx 
+        # four pointers:
+        # inorder start, inorder end, postorder start, postorder end
         def helper(ins, ine, pos, poe):
-            if ins > ine or pos > poe: return None
-            root = TreeNode(postorder[poe])
-            idx = hashmap[postorder[poe]]
-            no_elem_to_left = idx - ins
-            root.left =  helper(ins, ins + no_elem_to_left - 1, pos, pos + no_elem_to_left - 1)
-            root.right = helper(ins + no_elem_to_left + 1, ine, pos + no_elem_to_left, poe - 1)
-            return root
+            # base case
+            if ins > ine or pos > poe:
+                return None
+            # create a node with postorder_end value
+            node = TreeNode(postorder[poe])
+            # split the left and right children for node
+            idx = hm[postorder[poe]]
+            # get the number of elements for left child 
+            # we get this from inorder list
+            left = idx - ins
+            # set the left and right child of node
+            # left child: 
+            #   inorder: ins, ins - no of elements to left - 1(exclude middle element)
+            #   postorder: pos, pos + no of elements to left - 1(0 indexed)
+            # right child:
+            #   inorder:  ins - no of elements to left + 1, ine
+            #   postorder: pos + no of elements to left, poe - 1(exclude last element)
+
+            node.left = helper(ins, ins + left - 1, pos, pos + left - 1)
+            node.right = helper(ins + left + 1, ine, pos + left, poe - 1)
+            return node
         return helper(0, len(inorder) - 1, 0, len(postorder) - 1)
 
 def inorderTraversal(root):
