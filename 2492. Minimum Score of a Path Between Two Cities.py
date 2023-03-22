5a74a56032d5fb9f4eb42433b6510174aa93dc3e
shubh1007 <b200045@nitsikkim.ac.in>
2023-03-22 07:53:33 +0530

    DSA Daily Challenge

---
+++ 2492. Minimum Score of a Path Between Two Cities.py
@@ -0,0 +1,20 @@
+from sys import maxsize
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph = {}
+        for i in range(1, n + 1):
+            graph[i] = []
+        for start, end, cost in roads:
+            graph[start].append([end, cost])
+            graph[end].append([start, cost])
+        visited = set()
+        self.res = maxsize
+        def dfs(node):
+            self.res = min(self.res, node[1])
+            if node[0] in visited: return 
+            visited.add(node[0])
+            
+            for neighbour in graph[node[0]]:
+                dfs(neighbour)
+        dfs([1, maxsize])
+        return self.res
