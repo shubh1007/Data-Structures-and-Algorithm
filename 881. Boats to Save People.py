6e53c494a5cc7bc292b0dd916cd86ae7f71a5339
shubh1007 <b200045@nitsikkim.ac.in>
2023-04-03 08:29:00 +0530

    DSA Daily Challenge

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
+        res = 0
+        while left <= right:
+            SUM = people[left] + people[right]
+            if SUM <= limit:
+                res += 1
+                left += 1
+                right -= 1
+            else:
+                res += 1
+                right -= 1
+        return res
+
+
